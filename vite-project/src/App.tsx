122ace08309443296c99dce25aa1493f721d6b7f
Devesh Chaudhari <DeveshRx@users.noreply.github.com>
2025-05-15 08:53:15 +0000

    init

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+
+      <div>
+        <h1 className='text-3xl font-bold text-green-700'>Hello World</h1>
+        <Button>Click me</Button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
